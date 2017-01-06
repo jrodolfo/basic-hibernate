c3de5a0d28940f04239a16d75ae518318be9f690
Rod <jrodolfo@gmail.com>
2017-01-06 12:46:03 -0400

    added method that throws NonUniqueObjectException

--- src/main/java/com/jrodolfo/basichibernate/client/MessageApp.java
+++ src/main/java/com/jrodolfo/basichibernate/client/MessageApp.java
@@ -1,13 +1,10 @@
 package com.jrodolfo.basichibernate.client;
 
-import com.jrodolfo.basichibernate.dao.MessageDao;
-import com.jrodolfo.basichibernate.entity.Message;
 import com.jrodolfo.basichibernate.service.MessageService;
-import com.jrodolfo.basichibernate.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.NonUniqueObjectException;
 
-import java.util.List;
+import java.io.Serializable;
+
 
 /**
  * Created by Rod Oliveira (jrodolfo.com) on 2017-01-05.
@@ -19,11 +16,30 @@ public class MessageApp {
     static final String textThree = "text 3";
 
     public static void main(String[] args) {
+
         MessageService service = new MessageService();
         service.deleteAll();
         Long idOne = service.create(textOne);
         Long idTwo = service.create(textTwo);
         service.update(idTwo, textThree);
         service.delete(idOne);
+
+        // we want to create a situation where we get NonUniqueObjectException
+        try {
+            generateNonUniqueObjectException();
+        } catch (NonUniqueObjectException e) {
+            System.out.println("Catching a NonUniqueObjectException.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * We cannot have two instances referring to the same database column in one hibernate session.
+     * When that happens, the exception org.hibernate.NonUniqueObjectException is thrown.
+     * This method create a scenario to show this happening.
+     * @throws NonUniqueObjectException
+     */
+    public static void generateNonUniqueObjectException() throws NonUniqueObjectException {
+        throw new NonUniqueObjectException(new Serializable(){}, "Artificially creating an org.hibernate.NonUniqueObjectException exception.");
     }
 }
